5307270c9fa24e560f4c0a313dfc99dcc431aa20
Alejandro Bastidas <manuba2309@gmail.com>
2022-04-08 15:58:48 -0500

    Se creo el componente Boton con operador ternario y se crearon todos los numeros

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import logo_finca from './imagenes/fiinca.jpg'
+import Boton from './componentes/Boton'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img src={logo_finca} className="logo" alt="Logo pagina web"/>
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'> 
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+        <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>  
+        </div>
+        <div className='fila'>
+        <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'> 
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'> </div>
+      </div>
     </div>
   );
 }
